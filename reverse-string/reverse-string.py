869ab63d33927340291b75d00d82bf0abf269d25
SkySssw <71376730+SkySssw@users.noreply.github.com>
2024-07-01 19:53:49 +0700

    Time: 170 ms, Memory: 21 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -3,12 +3,12 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        wrtPtr = len(s) - 1
-    
-        half_len_s = int(len(s)/2)
-        for rdPtr in range(half_len_s):
-            rdAlp = s[rdPtr]
-            s[rdPtr] = s[wrtPtr]
-            s[wrtPtr] = rdAlp
+        rPtr = len(s) - 1
+        lPtr = 0
+        
+        while (rPtr>lPtr):
+            s[lPtr], s[rPtr] = s[rPtr], s[lPtr]
             
-            wrtPtr -= 1
+            rPtr -= 1
+            lPtr += 1
+       
