7c34ab1cab0da0c1c573cb917b32f1ecc34b5828
SkySssw <71376730+SkySssw@users.noreply.github.com>
2024-07-05 21:27:39 +0700

    Time: 114 ms, Memory: 19.1 MB - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,16 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        temp_set = set()
-        dup_list = []
+        single_num = 0
         
         for num in nums:
-            if num not in temp_set:
-                temp_set.add(num)
-            else:
-                dup_list.append(num)
-        
-        for num in nums:
-            if num not in dup_list:
-                return num
+            single_num ^= num
+        return single_num
             
