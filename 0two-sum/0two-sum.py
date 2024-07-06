b6107fa5a56ba78cf6fc2cc36825bd93cefd95a8
SkySssw <71376730+SkySssw@users.noreply.github.com>
2024-07-06 11:35:40 +0700

    Time: 46 ms, Memory: 17.7 MB - LeetHub

--- 0two-sum/0two-sum.py
+++ 0two-sum/0two-sum.py
@@ -1,7 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 1st Solution
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
         
+        store_diff = {}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+            num = nums[i]
+            if target-num in store_diff:
+                return [i, store_diff[target-num]]
+            store_diff[num] =  i
