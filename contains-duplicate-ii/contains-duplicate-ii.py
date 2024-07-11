25672a41e47aec53c608d9cad295be9da31e9822
SkySssw <71376730+SkySssw@users.noreply.github.com>
2024-07-11 17:24:27 +0700

    Time: 453 ms, Memory: 29.9 MB - LeetHub

--- contains-duplicate-ii/contains-duplicate-ii.py
+++ contains-duplicate-ii/contains-duplicate-ii.py
@@ -3,14 +3,12 @@ class Solution:
         
         dup_dict = {}
         for i,num in enumerate(nums):
-            if num not in dup_dict:
-                dup_dict[num] = [i]
-            else:
-                for prev_ind in dup_dict[num]:
-                    diff = abs(prev_ind - i)
-                    if diff <= k:
-                        return True
-                dup_dict[num].append(i)
+            
+            if num in dup_dict:
+                if i - dup_dict[num] <= k:
+                    return True
+            dup_dict[num] = i
+            
         
         return False
         
