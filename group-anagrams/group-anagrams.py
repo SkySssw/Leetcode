30f61c8b86a7d7fb750109212ecd55d455276b9a
SkySssw <71376730+SkySssw@users.noreply.github.com>
2024-07-12 22:13:38 +0700

    Time: 90 ms, Memory: 20.3 MB - LeetHub

---
+++ group-anagrams/group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        strs_dict = {}
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word not in strs_dict:
+                strs_dict[sorted_word]=[word]
+            else :
+                strs_dict[sorted_word].append(word)
+        
+        return strs_dict.values()
